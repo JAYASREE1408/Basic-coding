33741aa0d18376ba20dac18aa50804d7a39ade77
JAYASREE T <jayasreet2112@gmail.com>
2021-03-24 16:26:46 +0530

    Odd Numbers betweena range

---
+++ Odd Numbers betweena range.c
@@ -0,0 +1,15 @@
+//print odd numbers btw range.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,i;
+    printf("Enter the range to print odd numbers:\n");
+    scanf("%d %d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2!=0)
+            printf("%d ",i);
+    }
+}
