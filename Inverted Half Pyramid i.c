0d3f600cf4a3b6acc2a914aaffc55fdac191f439
JAYASREE T <jayasreet2112@gmail.com>
2021-03-24 20:14:09 +0530

    Inverted Half Pyramid priting i

---
+++ Inverted Half Pyramid i.c
@@ -0,0 +1,19 @@
+
+// Enter the number of elements: 5
+// 1 1 1 1 1 
+// 2 2 2 2 
+// 3 3 3 
+// 4 4 
+// 5 
+#include <stdio.h>
+int main(){
+	int n,i,j;
+	printf("Enter the number of elements: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n-i+1;j++){
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
+}
