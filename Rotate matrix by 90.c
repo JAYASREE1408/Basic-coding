783a31250069b0f3e4411472807c193c60ca4322
JAYASREE T <jayasreet2112@gmail.com>
2021-03-31 22:37:43 +0530

    Rotate matrix by 90

---
+++ Rotate matrix by 90.c
@@ -0,0 +1,51 @@
+/*
+Enter the matrix:                                                                                                             
+1 2 3                                                                                                                         
+4 5 6                                                                                                                         
+7 8 9                                                                                                                         
+Enter matrix after 90 degree rotation is:                                                                                     
+7 4 1                                                                                                                         
+8 5 2 
+9 6 3 
+*/
+#include<stdio.h>
+void main()
+{
+    int r,c,i,j;
+    printf("Enter the row and column :");
+    scanf("%d %d",&r,&c);
+    int a[r][c],temp[r][c];
+    printf("Enter the matrix: ");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            temp[i][j]=a[j][i];
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<c/2;j++)
+       {
+         int t=temp[i][j];
+         temp[i][j]=temp[i][c-j-1];
+         temp[i][c-j-1]=t;
+       }
+    }
+    printf("Enter matrix after 90 degree rotation is: \n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d ",temp[i][j]);
+        }
+        printf("\n");
+    }
+}
