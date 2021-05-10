bd080a03fbce673889c6b854f0d7982150456a7f
JAYASREE T <jayasreet2112@gmail.com>
2021-05-10 10:12:54 +0530

    Add files via upload

---
+++ Power of 3.c
@@ -0,0 +1,20 @@
+////Check whether the num is pow of 3
+//Enter the number:81
+//True
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+bool ispowerofThree(int n)
+{
+    double result= log10(n)/log10(3);
+    return result ==(int)result;
+}
+int main()
+{
+    int n,x;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    ispowerofThree(n)? printf("True"): printf("False");
+    return 0;
+}
