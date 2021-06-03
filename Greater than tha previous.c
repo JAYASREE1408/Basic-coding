963d42a9dd4f073dc07ca5a521516c9ad52b5e61
JAYASREE T <jayasreet2112@gmail.com>
2021-06-03 11:57:22 +0530

    Greater than tha previous

--- Greater than tha previous.c
+++ Greater than tha previous.c
@@ -1,9 +1,10 @@
 /*
-You’re given an array. Print the elements of the array which are greater than its previous elements
+Youâ€™re given an array. Print the elements of the array which are greater than its previous elements
 in the array.
 Input : 2, -3, -4, 5, 9, 7, 8 Output: 2 5 9
 You should solve this question in O(n) time.
 */
+
 #include<stdio.h>
 int main()
 {
