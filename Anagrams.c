1db32fbfdd35b2a67bb250d68307923ce3c71df0
JAYASREE T <jayasreet2112@gmail.com>
2021-06-12 23:03:48 +0530

    Anagrams

--- Anagrams.c
+++ Anagrams.c
@@ -1,3 +1,11 @@
+// An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. 
+// For example, the word anagram itself can be rearranged into nag a ram, also the word binary into brainy and the word adobe into abode. 
+// "New York Times" = "monkeys write"
+// "a gentleman" = "elegant man"
+// "eleven plus two" = "twelve plus one"
+// "William Shakespeare" = "I am a weakish speller"
+// "Madam Curie" = "Radium came"
+	
 #include<stdio.h>
 int main(){
 	char s1[20],s2[20];
